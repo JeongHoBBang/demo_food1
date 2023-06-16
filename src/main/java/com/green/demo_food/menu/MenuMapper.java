6125dd4d1c5203ed90e2854b7d13c0b5d5e9bac2
JeongHoBBang <dlawjdghbn@naver.com>
2023-06-16 14:26:13 +0900

    menu

--- src/main/java/com/green/demo_food/menu/MenuMapper.java
+++ src/main/java/com/green/demo_food/menu/MenuMapper.java
@@ -2,9 +2,11 @@ package com.green.demo_food.menu;
 
 import com.green.demo_food.menu.model.MenuEntity;
 import com.green.demo_food.menu.model.MenuInsDto;
+import com.green.demo_food.payment.model.PaymentEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MenuMapper {
     int insMenu(MenuInsDto dto);
+    int findPrice(PaymentEntity entity);
 }
